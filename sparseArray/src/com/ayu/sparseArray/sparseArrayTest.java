d0dc16469540aa7c836b024b5e0997e1b03cedbc
ayu <897682477@qq.com>
2019-12-04 11:18:56 +0800

    对源代码进行注释

--- sparseArray/src/com/ayu/sparseArray/sparseArrayTest.java
+++ sparseArray/src/com/ayu/sparseArray/sparseArrayTest.java
@@ -2,26 +2,34 @@ package com.ayu.sparseArray;
 
 import java.util.concurrent.ForkJoinPool;
 
+/**
+ * 当一个二维数组中大部分都是0的时候，称为“稀疏数组”，由于有很多重复的0，很浪费空间，故此使用压缩技术来压缩稀疏数组
+ */
 public class sparseArrayTest {
     public static void main(String[] args) {
+        //首先有一个11*11的数组，它占有的空间是121*4=484个字节
         int [][]arr= new int [11][11];
         arr[0][0]=1;
         arr[1][1]=12;
         arr[2][4]=88;
-        for(int row[]:arr){
+        //length用来记录不为0的个数
+        int length=0;
+        //对数组进行遍历，打印所有元素，可以看到有多少不是0的数字,当找到一个不为0的数字时，length的长度+1
+        for(int []row:arr){
             for(int temp:row){
                 System.out.printf("%d\t",temp);
+                if(temp!=0){
+                    length++;
+                }
             }
             System.out.println();
         }
-        int length=0;
-        for(int row[]:arr){
-            for(int temp:row){
-                if(temp!=0){
-                    length++;
-            }
-        }
-    }
+        /*
+                  对数组进行压缩，将稀疏数组转换成一个n*3的二维数组
+                  数组的第一行分别储存原数组的行数、列数、还有不为0的个数
+                  从第二行开始，第一列的元素代表不为0的数的原数组的行，第二列代表原数组的列，第三列代表不为0的具体指
+                  由于有length个不为0的数，第一行用来存原数组的行列信息，所以n=length+1
+         */
         int [][]zip= new int[length+1][3];
         zip[0][0]=arr.length;
         zip[0][1]=arr[0].length;
@@ -37,7 +45,8 @@ public class sparseArrayTest {
                 }
             }
         }
-        for(int row[]:zip){
+        //打印压缩后的数组，占有3*3*4=36个字节的空间，压缩率为36/484
+        for(int []row:zip){
             for(int elem:row){
                 System.out.printf("%d\t",elem);
             }
